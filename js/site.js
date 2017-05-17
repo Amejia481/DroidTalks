f4e7c7c964befeb936f20b3af4c8dcc52d988fb6
Arturo Mejia <arturomejiamarmol@gmail.com>
2017-05-17 13:47:29 -0400

    -Fixing url issue

--- js/site.js
+++ js/site.js
@@ -17,9 +17,7 @@ function writeUserData(url) {
     });
 }
 function showAlert(){
-    function isURL(value) {
-        return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
-    }
+
     swal({
             title: "Submit a new Resource!",
             type: "input",
@@ -28,8 +26,8 @@ function showAlert(){
             inputPlaceholder: "Write something"
         },
         function(inputValue){
-            if (!isURL(inputValue)) {
-                swal.showInputError("Invalid Url!");
+            if (!(inputValue)) {
+                swal.showInputError("You need to write something!");
                 return false
             }
 
